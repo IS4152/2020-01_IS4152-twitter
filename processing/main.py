6b9b83d23f0a131989b2a03302e356590a425adc
David Choo <7817101+davidchoo12@users.noreply.github.com>
2020-11-04 02:47:43 +0800

    processing script working

--- processing/main.py
+++ processing/main.py
@@ -1,5 +1,5 @@
-from os import path, makedirs
-import datetime
+from os import path, makedirs, environ
+from datetime import datetime, date, timedelta
 import logging
 import json
 import pandas as pd
@@ -34,39 +34,36 @@ class NumpyEncoder(json.JSONEncoder):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
 
-def process_tweets(df,text):
-    #stripping the common stop letters
-    text = re.sub(r'\@\w+|\#|\n|]b','', text)
-    text = text.translate(str.maketrans('', '', string.punctuation))
-    text = re.sub('([A-Z][a-z]+)', r' \1', text) #separattion of camelCase words to get more word tokens
-    text_tokens = word_tokenize(text)
-    filtered_words = [w for w in text_tokens if not w in stop_words] #removing of common stopwords
-    return " ".join(filtered_words)
-
-
 def main():
-    data_dir = '/mnt/d/Development/IS4152-twitter/data'
-    output_dir = '/mnt/d/Development/IS4152-twitter/output'
-    output_path = '/mnt/d/Development/IS4152-twitter/output/processed.json'
-    # today = datetime.date.today().isoformat()
-    today = '2020-10-18'
-    output_today_dir = path.join(output_dir, today)
-    makedirs(output_today_dir, exist_ok=True)
-    csv_file = path.join(data_dir, today + '.csv')
+    print('start ' + datetime.now().isoformat())
+    logger.info('start ' + datetime.now().isoformat())
+    data_dir = environ.get('DATA_DIR', '/home/dc/IS4152-twitter/data')
+    output_dir = environ.get('OUTPUT_DIR', '/home/dc/IS4152-twitter/output')
+    output_path = path.join(output_dir, 'processed.json')
+    yesterday = (date.today() - timedelta(days=1)).isoformat()
+    #yesterday = '2020-10-24'
+    print('processing ' + yesterday)
+    logger.info('processing ' + yesterday)
+    output_yesterday_dir = path.join(output_dir, yesterday)
+    makedirs(output_yesterday_dir, exist_ok=True)
+    csv_file = path.join(data_dir, yesterday + '.csv')
 
-    df = pd.read_csv(csv_file)
-    tweets = df['text'].values.tolist()
-    topics_word_freq = topic_model(tweets, output_today_dir)
-    output_json = []
+    df = pd.read_csv(csv_file, usecols=['text', 'sarcasm_value', 'created_at'])
 
     #remove sarcastic tweets
-    df = df[df['sarcasm_value']<0] #takes value less than 0 which are non-saracastic tweets
+    df = df[df['sarcasm_value'] != 'error']
+    df['sarcasm_value'] = pd.to_numeric(df['sarcasm_value'])
+    df = df[df['sarcasm_value'] < 0] #takes value less than 0 which are non-saracastic tweets
+
+    tweets = df['text'].values.tolist()
+    topics_word_freq = topic_model(tweets, output_yesterday_dir)
+    output_json = []
+
+    random_tweets = df.sample(10).replace({np.nan: None}).to_dict('records')
 
     #process the tweets
-    processed = []
-    for i in range(df.shape[0]):
-        processed.append(process_tweets(df.loc[i,'text']))
-    df['processed_tweets'] = processed
+    processed = process_tweets(tweets)
+    df['tweets'] = processed
 
     df = score_tweets(df)
     df = separate_scores(df)
@@ -82,8 +79,7 @@ def main():
     trump_df_grp = trump_df_grp.reset_index()
     trump_df_grp = trump_df_grp[['created_at_hr','sarcasm_value','compounded','neutral','positive','negative']]
     trump_df_grp['hour'] = [t.hour for t in trump_df_grp['created_at_hr']]
-    trump_hour = trump_df_grp.to_dict('records')
-    trump_hour = next((item for item in trump_df_grp if item.get('hour') == hour), {})
+    trump_df_list = trump_df_grp.to_dict('records')
 
 
     biden_df['created_at_hr'] = pd.to_datetime(biden_df['created_at'])
@@ -92,30 +88,44 @@ def main():
     biden_df_grp = biden_df_grp.reset_index()
     biden_df_grp = biden_df_grp[['created_at_hr','sarcasm_value','compounded','neutral','positive','negative']]
     biden_df_grp['hour'] = [t.hour for t in biden_df_grp['created_at_hr']]
-    biden_df_grp = biden_df_grp.to_dict('records')
-    biden_hour = next((item for item in biden_df_grp if item.get('hour') == hour), {})
+    biden_df_list = biden_df_grp.to_dict('records')
 
-    combined = { 'hour': hour, 'biden': biden_hour, 'trump': trump_hour }
-    sentiment =[]
-    sentiment.append(combined)
+    sentiment = []
+    for hour in range(24):
+        biden_hour = next((item for item in biden_df_list if item.get('hour') == hour), {})
+        trump_hour = next((item for item in trump_df_list if item.get('hour') == hour), {})
+        biden_hour.pop('hour', None)
+        biden_hour.pop('created_at_hr', None)
+        trump_hour.pop('hour', None)
+        trump_hour.pop('created_at_hr', None)
+        combined = { 'hour': hour, 'biden': biden_hour, 'trump': trump_hour }
+        sentiment.append(combined)
+
     try:
         with open(output_path, 'r') as output_file:
             output_json = json.load(output_file)
     except Exception as e:
         logger.error(e)
-    # find today's output if any
-    output_today = next((item for item in output_json if item['date'] == today), None)
-    if not output_today:
-        output_today = { 'date': today }
-        output_json.append(output_today)
-    output_today['topics'] = topics_word_freq # stringify numpy float32
-    output_today['sentiment'] = sentiment # stringify numpy float32
+    # find yesterday's output if any
+    output_yesterday = next((item for item in output_json if item['date'] == yesterday), None)
+    if not output_yesterday:
+        output_yesterday = { 'date': yesterday }
+        output_json.append(output_yesterday)
+    output_yesterday['topics'] = topics_word_freq # stringify numpy float32
+    output_yesterday['sentiments'] = sentiment
+    output_yesterday['tweets'] = random_tweets
+    output_json.sort(key=lambda x: x['date'])
     try:
         with open(output_path, 'w') as output_file:
             # update output json
-            json.dump(output_json, output_file, cls=NumpyEncoder)
+            json.dump(output_json, output_file, cls=NumpyEncoder, separators=(',', ':'))
     except Exception as e:
         logger.error(e)
+    logger.info('end ' + datetime.now().isoformat())
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as e:
+        logger.error('GLOBAL EXCEPTION')
+        logger.error(e)
--- processing/sentiment_analysis.py
+++ processing/sentiment_analysis.py
@@ -8,17 +8,21 @@ from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
+import numpy as np
 import json
 stop_words = set(stopwords.words('english')) 
 
-def process_tweets(text):
-    #stripping the common stop letters
-    text = re.sub(r'\@\w+|\#|\n|]b','', text)
-    text = text.translate(str.maketrans('', '', string.punctuation))
-    text = re.sub('([A-Z][a-z]+)', r' \1', text) #separattion of camelCase words to get more word tokens
-    text_tokens = word_tokenize(text)
-    filtered_words = [w for w in text_tokens if not w in stop_words] #removing of common stopwords
-    return " ".join(filtered_words)
+def process_tweets(texts):
+    processed = []
+    for text in texts:
+        #stripping the common stop letters
+        text = re.sub(r'\@\w+|\#|\n|]b','', text)
+        text = text.translate(str.maketrans('', '', string.punctuation))
+        text = re.sub('([A-Z][a-z]+)', r' \1', text) #separattion of camelCase words to get more word tokens
+        text_tokens = word_tokenize(text)
+        filtered_words = [w for w in text_tokens if not w in stop_words] #removing of common stopwords
+        processed.append(" ".join(filtered_words))
+    return processed
 
 
 
@@ -26,7 +30,7 @@ def score_tweets(df):
     sa = SentimentIntensityAnalyzer()
     scores=[]
     for i in range(df.shape[0]):
-        scores.append(sa.polarity_scores(df.iloc[i]['processed_tweets']))
+        scores.append(sa.polarity_scores(df.iloc[i]['tweets']))
     df['polarity_scores'] = scores
     return df
 
@@ -38,11 +42,14 @@ def separate_scores(df):
     return df
 
 def label_candidates(df):
-    match = ['trump','#trump','rep','republican']
-    df['candidate'] = 'NA'
-    for i in range(df.shape[0]):
-        if any(x in df.iloc[i]['text'].lower() for x in match):
-            df.loc[i,'candidate'] = 'Trump'
-        else: 
-            df.loc[i,'candidate'] = 'Biden'
+    biden_count = df['text'].str.lower().str.count('biden')
+    trump_count = df['text'].str.lower().str.count('trump')
+    # set dataframe column conditionally based on series, src: https://stackoverflow.com/a/19913845/4858751
+    conditions = [
+        biden_count > trump_count,
+        trump_count > biden_count
+    ]
+    choices = ['Biden', 'Trump']
+    df['candidate'] = np.select(conditions, choices, default='NA')
+    print(df['candidate'].head())
     return df
